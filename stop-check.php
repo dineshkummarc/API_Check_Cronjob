97e2d20c6b981190c6d3ae2fd970b8b6a32079cc
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-09 10:15:50 +0530

    update

--- stop-check.php
+++ stop-check.php
@@ -2,12 +2,12 @@
 
 	//Set state = 0 file
 	function set_state_stop(){
-		$myFile = "/home/hvn0220437/supercleaner.info/public_html/state.txt";
+		$myFile = "state.txt";
 		$fh = fopen($myFile, 'w+') or die("Cannot open file");
 		$myFileContents = 0;
 		fwrite($fh, $myFileContents);
 		fclose($fh);
-		$myFile2 = "/home/hvn0220437/supercleaner.info/public_html/output.txt";
+		$myFile2 = "output.txt";
 		$fh2 = fopen($myFile2, 'a') or die("Cannot append file");
 		$annouce = "Đã dừng chạy!"."<br>";
 		fwrite($fh2, $annouce);
@@ -15,5 +15,5 @@
 	}
 
 	set_state_stop();
-	header("Location: /checkapi.php?action=success");
+	header("Location: checkapi.php?action=success");
 ?>
