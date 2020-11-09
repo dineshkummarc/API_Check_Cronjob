282c31d320a392ca3d04242f14cff7195c979c24
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2020-11-09 10:14:36 +0530

    update

--- continue-check.php
+++ continue-check.php
@@ -2,12 +2,12 @@
 
 	//Set state = 1 file
 	function set_state_start(){
-		$myFile = "/home/hvn0220437/supercleaner.info/public_html/state.txt";
+		$myFile = "state.txt";
 		$fh = fopen($myFile, 'w+') or die("Cannot open file");
 		$myFileContents = 1;
 		fwrite($fh, $myFileContents);
 		fclose($fh);
-		$myFile2 = "/home/hvn0220437/supercleaner.info/public_html/output.txt";
+		$myFile2 = "output.txt";
 		$fh2 = fopen($myFile2, 'a') or die("Cannot append file");
 		$annouce = "Bắt đầu chạy!"."<br>";
 		fwrite($fh2, $annouce);
@@ -15,5 +15,5 @@
 	}
 
 	set_state_start();
-	header("Location: /checkapi.php?action=success");
+	header("Location: checkapi.php?action=success");
 ?>
